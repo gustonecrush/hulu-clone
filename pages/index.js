3833673ca80cdcc84b4499134cf0b35d5e9af22b
gustonecrush <codeboxezz@gmail.com>
2022-12-01 18:47:02 +0700

    edited favicon

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home({ results }) {
     <div className="">
       <Head>
         <title>Hulu Clone</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
